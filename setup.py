d7fe949892d6517b440ca2896e0a630798e46326
dagun2 <2lian@naver.com>
2025-05-05 00:17:42 +0900

    Update setup.py
     'LSEnvironment': {
            'PYTHONIOENCODING': 'utf-8',
            'LANG': 'en_US.UTF-8',
            'LC_ALL': 'en_US.UTF-8'
        }추가

--- setup.py
+++ setup.py
@@ -17,6 +17,11 @@ OPTIONS = {
         'CFBundleVersion': '1.0.0',
         'CFBundleShortVersionString': '1.0.0',
         'NSHighResolutionCapable': True,
+        'LSEnvironment': {
+            'PYTHONIOENCODING': 'utf-8',
+            'LANG': 'en_US.UTF-8',
+            'LC_ALL': 'en_US.UTF-8'
+        }
     },
     # 'iconfile': 'icon.icns',
 }
